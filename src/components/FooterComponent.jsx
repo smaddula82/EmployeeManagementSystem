4fe73f831729cb6fa7e317be7d522cfdc71de67d
smaddula82 <subrahmanyam.maddula@gmail.com>
2023-08-03 13:28:24 +0530

    updated for add employee

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <div>
+                <footer className='footer'>
+                    <span className='textMuted'>CopyRight@2023</span>
+
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
